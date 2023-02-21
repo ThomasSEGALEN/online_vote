6003402e45a84a677f5a9d1ce6151759308f031b
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-02-21 16:13:04 +0100

    Default parameters in userForm

--- resources/js/Composables/userForm.ts
+++ resources/js/Composables/userForm.ts
@@ -1,15 +1,23 @@
 import { useForm } from "@inertiajs/vue3";
 
-export default function userForm() {
+export default function userForm(
+    civility = 1,
+    last_name = "",
+    first_name = "",
+    email = "",
+    role = 2,
+    groups = [],
+    permissions = []
+) {
     const form = useForm({
-        civility: 1 as number,
-        last_name: "" as string,
-        first_name: "" as string,
-        email: "" as string,
+        civility: civility as number,
+        last_name: last_name as string,
+        first_name: first_name as string,
+        email: email as string,
         password: "" as string,
-        role: 2 as number,
-        groups: [] as Array<number>,
-        permissions: [] as Array<number>,
+        role: role as number,
+        groups: groups as Array<number>,
+        permissions: permissions as Array<number>,
     });
 
     return form;
