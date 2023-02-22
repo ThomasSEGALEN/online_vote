44e94a022364c1fc48bfd23d21d0e872036d3263
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-02-22 13:55:56 +0100

    Excel download/upload unit tests

---
+++ tests/Unit/ExcelTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Exports\UserExport;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ExcelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_excel_download()
+    {
+        $user = User::factory()->create();
+
+        Excel::fake();
+
+        $response = $this->actingAs($user)->get('/users/export');
+
+        $response->assertOk();
+
+        Excel::assertDownloaded('users.xlsx');
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_excel_upload()
+    {
+        $user = User::factory()->create();
+        $file = UploadedFile::fake()->create('users.xlsx');
+
+        Excel::fake();
+
+        $response = $this->actingAs($user)->post('/users/import', ['file' => $file]);
+
+        $response->assertFound();
+
+        Excel::assertImported('users.xlsx');
+    }
+}
