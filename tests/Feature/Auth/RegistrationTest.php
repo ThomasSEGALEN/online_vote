32e4aa207e1b38f6b09c1ca559f008c7cfa163d8
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-02-22 10:31:02 +0100

    Fix tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -20,7 +20,9 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register(): void
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'civility' => 1,
+            'last_name' => 'Test',
+            'first_name' => 'User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
