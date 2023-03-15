6c3e0101a42901b1326be56330c8405756dda725
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2023-03-15 09:59:49 +0100

    Fix layout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -10,18 +10,16 @@ import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 </script>
 
 <template>
-    <div>
-        <div class="flex flex-col md:flex-row">
-            <Navbar />
+    <div class="flex flex-col md:flex-row">
+        <Navbar />
 
-            <main class="flex-1 md:ml-64">
-                <header class="" v-if="$slots.header">
-                    <div class="py-4 px-4 h-14 border-b">
-                        <slot name="header" />
-                    </div>
-                </header>
-                <slot />
-            </main>
-        </div>
+        <main class="flex flex-col w-full">
+            <header v-if="$slots.header">
+                <div class="py-4 px-4 h-14 border-b">
+                    <slot name="header" />
+                </div>
+            </header>
+            <slot />
+        </main>
     </div>
 </template>
