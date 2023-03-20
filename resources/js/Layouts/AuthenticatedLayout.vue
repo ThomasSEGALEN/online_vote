e96043ef643495c82cff2103cde639e0048cba5b
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-03-20 13:38:21 +0100

    Remove unused imports

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { usePage } from "@inertiajs/vue3";
-import route from "ziggy-js";
-import Dropdown from "@/Components/Dropdown.vue";
-import DropdownLink from "@/Components/DropdownLink.vue";
 import Navbar from "@/Components/Navbar.vue";
-import NavLink from "@/Components/NavLink.vue";
-import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 </script>
 
 <template>
     <div class="flex flex-col md:flex-row">
         <Navbar />
 
-        <main class="flex flex-col w-full md:ml-64">
+        <main class="flex flex-col w-full md:ml-56">
             <header v-if="$slots.header">
                 <div class="py-4 px-4 h-14 border-b">
                     <slot name="header" />
