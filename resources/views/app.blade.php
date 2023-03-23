9de3e0d68a5eddff49bae79095be52cc8a470e70
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-03-23 10:31:02 +0100

    Fix responsive css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-w-fit">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
@@ -19,6 +18,12 @@
     </body>
 </html>
 <style>
+html, body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
 ::selection {
     color: white;
     background-color: rgb(99 102 241);
