4cca1163ccfae5531414f8708120673364cd349b
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-03-23 10:51:14 +0100

    Update controllers of export/import routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\ExportImportController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return inertia('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -38,22 +38,28 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::post('/users/import', [UserController::class, 'import'])->name('users.import');
-    Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
+    Route::post('/users/import', [ExportImportController::class, 'importUsers'])->name('users.import');
+    Route::get('/users/export', [ExportImportController::class, 'exportUsers'])->name('users.export');
 
-    // TODO: CRUD Role/Group
     Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
     Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
     Route::post('/roles/store', [RoleController::class, 'store'])->name('roles.store');
     Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
     Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
     Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
-    Route::post('/roles/import', [RoleController::class, 'import'])->name('roles.import');
-    Route::get('/roles/export', [RoleController::class, 'export'])->name('roles.export');
+    Route::post('/roles/import', [ExportImportController::class, 'importRoles'])->name('roles.import');
+    Route::get('/roles/export', [ExportImportController::class, 'exportRoles'])->name('roles.export');
 
     Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
+    Route::get('/groups/create', [GroupController::class, 'create'])->name('groups.create');
+    Route::post('/groups/store', [GroupController::class, 'store'])->name('groups.store');
+    Route::get('/groups/{group}/edit', [GroupController::class, 'edit'])->name('groups.edit');
+    Route::put('/groups/{group}', [GroupController::class, 'update'])->name('groups.update');
+    Route::delete('/groups/{group}', [GroupController::class, 'destroy'])->name('groups.destroy');
+    Route::post('/groups/import', [GroupController::class, 'import'])->name('groups.import');
+    Route::get('/groups/export', [GroupController::class, 'export'])->name('groups.export');
 
     Route::get('/sessions', fn () => to_route('users.index'))->name('sessions.index');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
