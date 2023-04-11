ae0770d8f183f123cd2b5e88afdf691eb57978d4
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-04-11 14:18:33 +0200

    Create group factory

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -5,16 +5,16 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
  */
-class RoleFactory extends Factory
+class GroupFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => fake()->jobTitle(),
