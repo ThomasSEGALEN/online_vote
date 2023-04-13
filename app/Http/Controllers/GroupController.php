a2934e54ce183460825b5d923883246ab939c4cf
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-04-13 11:00:06 +0200

    Remove unused import

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GroupStoreRequest;
 use App\Models\Group;
-use App\Models\User;
 use App\Services\GroupService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
