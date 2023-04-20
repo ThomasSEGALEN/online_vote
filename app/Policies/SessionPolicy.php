191bb4771f7bb92644d17b9af3a21339d15410db
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-04-20 14:57:02 +0200

    Add handles authorization trait

--- app/Policies/SessionPolicy.php
+++ app/Policies/SessionPolicy.php
@@ -4,10 +4,12 @@ namespace App\Policies;
 
 use App\Models\Session;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class SessionPolicy
 {
+    use HandlesAuthorization;
+
     /**
      * Determine whether the user can view any models.
      *
