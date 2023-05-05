66c7012e11712f32c196cd38712a61d05dba12ab
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-05-05 11:15:14 +0200

    Update string length of remember token

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'first_name' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(100),
             'civility_id' => rand(1, 2),
             'role_id' => 2,
             'created_at' => date('Y-m-d H:i:s'),
