8164efb4935fd636f3e94624e4667571633bcad5
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-05-12 08:53:31 +0200

    Remove unused props

--- resources/js/Pages/Sessions/Index.vue
+++ resources/js/Pages/Sessions/Index.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from "vue";
-import { Head, Link, router, usePage } from "@inertiajs/vue3";
+import { computed, nextTick, onMounted, onUpdated, ref, watch } from "vue";
+import { Head, Link, router, useForm, usePage } from "@inertiajs/vue3";
 import route from "ziggy-js";
-import { throttle } from "lodash";
+import { isEmpty, throttle } from "lodash";
+import AnswerIcon from "@/Components/AnswerIcon.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import CaretUpIcon from "@/Components/CaretUpIcon.vue";
+import CaretDownIcon from "@/Components/CaretDownIcon.vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import ColorInput from "@/Components/ColorInput.vue";
 import DangerButton from "@/Components/DangerButton.vue";
 import DeleteIcon from "@/Components/DeleteIcon.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
 import Modal from "@/Components/Modal.vue";
+import NumberInput from "@/Components/NumberInput.vue";
 import Pagination from "@/Components/Pagination.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 import ResponsivePagination from "@/Components/ResponsivePagination.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 import UpdateIcon from "@/Components/UpdateIcon.vue";
 
 const props = defineProps({
-    showCreateModal: Boolean,
-    showViewModal: Boolean,
-    showDeleteModal: Boolean,
     sessions: {
         type: Object,
         default: () => {
             return {};
         },
+    },
+    labelSets: {
+        type: Array<LabelSet>,
+        default: () => [],
     },
     statuses: {
         type: Array<Status>,
@@ -42,14 +52,34 @@ const props = defineProps({
 });
 
 const search = ref<string>(props.filters?.search);
+const confirmingAnswerAction = ref<boolean>(false);
+const confirmingAnswerCreation = ref<boolean>(false);
+const confirmingAnswerDeletion = ref<boolean>(false);
+const labelCreationInput = ref<HTMLInputElement>();
 const confirmingSessionDeletion = ref<boolean>(false);
 const sessionId = ref<number>();
 const showMessage = ref<boolean>(false);
+const currentAnswer = ref<number>(0);
 
 const successMessage = computed(() => usePage().props.flash.success);
 const errorMessage = computed(() => usePage().props.flash.error);
 
+const form = useForm({
+    label: "",
+    amount: 1,
+    names: [],
+    colors: [],
+    labelSets: [],
+});
+
 onMounted(() => {
+    showMessage.value = true;
+    setTimeout(() => {
+        showMessage.value = false;
+    }, 3000);
+});
+
+onUpdated(() => {
     showMessage.value = true;
     setTimeout(() => {
         showMessage.value = false;
@@ -67,18 +97,48 @@ watch(
     }, 500)
 );
 
+const confirmAnswerAction = () => (confirmingAnswerAction.value = true);
+const confirmAnswerCreation = () => {
+    closeAnswerActionModal();
+    confirmingAnswerCreation.value = true;
+    nextTick(() => labelCreationInput.value?.focus());
+};
+const confirmAnswerDeletion = () => {
+    closeAnswerActionModal();
+    confirmingAnswerDeletion.value = true;
+};
+
 const confirmSessionDeletion = (id: number) => {
     confirmingSessionDeletion.value = true;
     sessionId.value = id;
 };
 
+const createAnswer = () =>
+    form.post(route("labelSets.store"), {
+        onSuccess: () => closeAnswerCreationModal(),
+        onFinish: () => form.reset(),
+    });
+const deleteAnswer = () => {
+    form.labelSets.map((id: number) =>
+        form.delete(route("labelSets.destroy", id))
+    );
+    form.reset();
+    closeAnswerDeletionModal();
+};
+console.log(props.labelSets);
+
 const deleteSession = () => {
     router.delete(`sessions/${sessionId.value}`, {
-        preserveScroll: true,
-        preserveState: false,
         onSuccess: () => closeModal(),
     });
 };
+
+const closeAnswerActionModal = () => (confirmingAnswerAction.value = false);
+const closeAnswerCreationModal = () => {
+    form.reset();
+    confirmingAnswerCreation.value = false;
+};
+const closeAnswerDeletionModal = () => (confirmingAnswerDeletion.value = false);
 
 const closeModal = () => (confirmingSessionDeletion.value = false);
 </script>
@@ -93,21 +153,257 @@ const closeModal = () => (confirmingSessionDeletion.value = false);
             </h2>
         </template>
 
+        <Modal :show="confirmingAnswerAction" @close="closeAnswerActionModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-800">
+                    Jeu d'étiquettes
+                </h2>
+
+                <div
+                    class="mt-6 grid gap-4 grid-cols-1 xs:flex xs:flex-row xs:justify-evenly place-items-center"
+                >
+                    <button
+                        class="w-32 h-32 bg-gray-100 border rounded-xl"
+                        @click="confirmAnswerCreation"
+                    >
+                        Créer
+                    </button>
+
+                    <button
+                        class="w-32 h-32 bg-gray-100 border rounded-xl"
+                        @click="confirmAnswerDeletion"
+                    >
+                        Supprimer
+                    </button>
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="closeAnswerActionModal">
+                        Annuler
+                    </SecondaryButton>
+                </div>
+            </div>
+        </Modal>
+
+        <Modal
+            :show="confirmingAnswerCreation"
+            @close="closeAnswerCreationModal"
+        >
+            <form class="p-6 mx-20" @submit.prevent="createAnswer">
+                <h2 class="text-lg font-medium text-gray-800">
+                    Création d'un jeu d'étiquettes
+                </h2>
+
+                <div class="mt-6 flex flex-col md:flex-row justify-between">
+                    <div>
+                        <InputLabel for="label" value="Label" />
+
+                        <TextInput
+                            id="label"
+                            ref="labelCreationInput"
+                            v-model="form.label"
+                            type="text"
+                            class="mt-1 block w-full"
+                            required
+                        />
+
+                        <InputError class="mt-2" :message="form.errors.label" />
+                    </div>
+
+                    <div class="mt-4 md:mt-0">
+                        <InputLabel for="amount" value="Nombre de réponses" />
+
+                        <NumberInput
+                            id="amount"
+                            v-model.number="form.amount"
+                            :min="1"
+                            :max="20"
+                            class="mt-1 block w-full"
+                            required
+                        />
+
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.amount"
+                        />
+                    </div>
+                </div>
+
+                <div
+                    v-for="(amount, index) in form.amount"
+                    :key="index"
+                    class="mt-4"
+                >
+                    <InputLabel
+                        :for="`answer-${index}`"
+                        :value="`Réponse ${index + 1}`"
+                    />
+
+                    <div class="flex items-center justify-between space-x-4">
+                        <TextInput
+                            :id="`answer-${index}`"
+                            v-model="form.names[index]"
+                            type="text"
+                            class="mt-1 block w-full"
+                            required
+                        />
+
+                        <ColorInput v-model="form.colors[index]" />
+                    </div>
+
+                    <InputError
+                        class="mt-2"
+                        :message="form.errors[`colors.${index}` as keyof object]"
+                    />
+                </div>
+
+                <div
+                    class="mt-6 inline-flex md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 justify-end"
+                >
+                    <SecondaryButton @click="closeAnswerCreationModal">
+                        Annuler
+                    </SecondaryButton>
+
+                    <PrimaryButton
+                        :class="{
+                            'opacity-25': form.processing,
+                        }"
+                        :disabled="form.processing"
+                    >
+                        Créer
+                    </PrimaryButton>
+                </div>
+            </form>
+        </Modal>
+
+        <Modal
+            :show="confirmingAnswerDeletion"
+            @close="closeAnswerDeletionModal"
+        >
+            <form class="p-6 mx-20" @submit.prevent="deleteAnswer">
+                <h2 class="text-lg font-medium text-gray-800">
+                    Suppression d'un jeu d'étiquettes
+                </h2>
+
+                <div class="mt-6 flex flex-col md:flex-row justify-between">
+                    <div v-if="labelSets.length">
+                        <div
+                            v-for="(labelSet, index) in labelSets"
+                            :key="index"
+                        >
+                            <div class="flex flex-row items-center">
+                                <Checkbox
+                                    :id="`labelSets-${index}`"
+                                    v-model="form.labelSets"
+                                    :value="labelSet.id"
+                                    class="mr-1"
+                                />
+
+                                <button
+                                    type="button"
+                                    @click="
+                                        currentAnswer !== index
+                                            ? (currentAnswer = index)
+                                            : (currentAnswer = -1)
+                                    "
+                                >
+                                    <div class="inline-flex items-center">
+                                        <span
+                                            class="block font-medium text-md text-gray-700"
+                                        >
+                                            {{ labelSet.name }}
+                                        </span>
+
+                                        <div class="ml-1">
+                                            <template
+                                                v-if="currentAnswer === index"
+                                            >
+                                                <CaretUpIcon />
+                                            </template>
+
+                                            <template v-else>
+                                                <CaretDownIcon />
+                                            </template>
+                                        </div>
+                                    </div>
+                                </button>
+                            </div>
+
+                            <div v-show="currentAnswer === index">
+                                <div>
+                                    <span
+                                        class="block font-medium text-md text-gray-700"
+                                    >
+                                        Réponses :
+                                    </span>
+
+                                    <ul class="max-h-36 overflow-y-auto">
+                                        <li
+                                            v-for="(
+                                                answer, answerIndex
+                                            ) in labelSet.answers"
+                                            :key="answerIndex"
+                                        >
+                                            <span
+                                                class="block font-medium text-sm text-gray-700"
+                                            >
+                                                {{ answer.name }}
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <span v-else class="block font-medium text-md text-gray-700"
+                        >Aucun jeu d'étiquettes enregistré</span
+                    >
+                </div>
+
+                <div
+                    class="mt-6 inline-flex md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 justify-end"
+                >
+                    <SecondaryButton @click="closeAnswerDeletionModal">
+                        Annuler
+                    </SecondaryButton>
+
+                    <PrimaryButton
+                        v-if="!isEmpty(labelSets)"
+                        :class="{
+                            'opacity-25': form.processing,
+                        }"
+                        :disabled="form.processing"
+                    >
+                        Supprimer
+                    </PrimaryButton>
+                </div>
+            </form>
+        </Modal>
+
         <div class="p-4 md:p-6">
-            <div class="flex flex-wrap flex-row items-center justify-between">
+            <div class="flex flex-wrap flex-row justify-between">
                 <div
                     v-if="can.createSessions"
                     class="flex items-center space-x-2 mb-2"
                 >
                     <Link
-                        class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-bold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 active:bg-indigo-700 focus:ring-offset-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-150 ease-in-out"
+                        class="inline-flex px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-bold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 active:bg-indigo-700 focus:ring-offset-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-150 ease-in-out"
                         :href="route('sessions.create')"
                     >
                         Nouvelle séance
                     </Link>
+
+                    <button
+                        class="inline-flex px-2 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
+                        @click="confirmAnswerAction"
+                    >
+                        <AnswerIcon />
+                    </button>
                 </div>
 
                 <TextInput
+                    id="search"
                     v-model="search"
                     class="block mb-2"
                     type="text"
@@ -232,6 +528,7 @@ const closeModal = () => (confirmingSessionDeletion.value = false);
                                 >
                                     <UpdateIcon />
                                 </Link>
+
                                 <DangerButton
                                     v-if="can.deleteSessions"
                                     @click="confirmSessionDeletion(session.id)"
@@ -282,3 +579,5 @@ const closeModal = () => (confirmingSessionDeletion.value = false);
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
