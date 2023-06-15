75b5a08d14d489c206af609681fce69b37af9629
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-06-15 10:02:38 +0200

    Update responsive limits

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -3,10 +3,10 @@ import Navbar from "@/Components/Navbar.vue";
 </script>
 
 <template>
-    <div class="flex flex-col md:flex-row">
+    <div class="flex flex-col lg:flex-row">
         <Navbar />
 
-        <main class="flex flex-col flex-1 md:ml-56">
+        <main class="flex flex-col flex-1 lg:ml-56">
             <header v-if="$slots.header">
                 <div class="py-4 px-4 h-fit border-b">
                     <slot name="header" />
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,7 @@ import { ref, watch } from "vue";
 import { Head, Link, router, usePage } from "@inertiajs/vue3";
 import route from "ziggy-js";
 import { throttle } from "lodash";
+import { Permission, Session, Status } from "@/types/types";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import RadioInput from "@/Components/RadioInput.vue";
@@ -57,7 +58,7 @@ const hasAccess = (session: Session): boolean =>
             </h2>
         </template>
 
-        <div class="p-4 md:p-6 space-y-6">
+        <div class="p-4 lg:p-6">
             <div>
                 <span class="block font-medium text-md text-gray-700">
                     Filtre
@@ -97,38 +98,51 @@ const hasAccess = (session: Session): boolean =>
             </div>
 
             <!-- {{ (sessions.data as Array<Session>).filter((data) => data.status_id === 2) }} -->
-            <div
-                v-for="session in sessions.data as Array<Session>"
-                :key="session.id"
-            >
-                <Link :href="route('sessions.show', session.id)">
-                    <div
-                        v-show="hasAccess(session)"
-                        class="bg-white p-6 rounded-lg shadow-lg"
-                    >
-                        <div class="flex justify-between">
-                            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-                                {{ session.title }}
-                            </h2>
-                            <span
-                                class="text-md mb-2 text-gray-800"
-                                :class="
-                                    session.status_id === 1
-                                        ? 'text-green-500'
-                                        : 'text-red-500'
-                                "
-                            >
-                                {{
-                                    session.status_id === 1 ? "Ouvert" : "Fermé"
-                                }}
-                            </span>
-                        </div>
+            <div class="flex flex-col space-y-6 mt-4">
+                <template
+                    v-for="session in sessions.data as Array<Session>"
+                    :key="session.id"
+                >
+                    <Link :href="route('sessions.show', session.id)">
+                        <div
+                            v-show="hasAccess(session)"
+                            class="flex flex-col bg-white p-6 rounded-lg shadow-lg"
+                        >
+                            <div class="flex flex-wrap justify-between">
+                                <h2
+                                    class="text-2xl font-bold mb-2 text-gray-800"
+                                >
+                                    {{ session.title }}
+                                </h2>
+                                <span
+                                    class="text-md mb-2 text-gray-800"
+                                    :class="
+                                        session.status_id === 1
+                                            ? 'text-green-500'
+                                            : 'text-red-500'
+                                    "
+                                >
+                                    {{
+                                        session.status_id === 1
+                                            ? "Ouvert"
+                                            : "Fermé"
+                                    }}
+                                </span>
+                            </div>
 
-                        <p class="text-gray-700">
-                            {{ session.description }}
-                        </p>
-                    </div>
-                </Link>
+                            <p class="text-gray-700 break-all">
+                                {{
+                                    session.description.length > 100
+                                        ? session.description.substring(
+                                              0,
+                                              100
+                                          ) + "..."
+                                        : session.description
+                                }}
+                            </p>
+                        </div>
+                    </Link>
+                </template>
             </div>
         </div>
     </AuthenticatedLayout>
