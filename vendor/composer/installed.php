4f826375b9720b71e62edc1df2a9a219db6ecbfc
ThomasSEGALEN <t.segalen@dcsin.lan>
2023-06-23 11:55:11 +0200

    Composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '6f419af5378f492726e813952c2414102794b0a5',
+        'reference' => 'a643ec50ed055b1fff0d1c98f3828d3945e15b87',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -418,7 +418,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '6f419af5378f492726e813952c2414102794b0a5',
+            'reference' => 'a643ec50ed055b1fff0d1c98f3828d3945e15b87',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -796,8 +796,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
